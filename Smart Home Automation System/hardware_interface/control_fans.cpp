75c57a31ad89195ab3737d54072ee16fecf140c0
rahaan123 <85208811+rahaan123@users.noreply.github.com>
2025-05-18 18:40:28 -0500

    Update control_fans.cpp

--- Smart Home Automation System/hardware_interface/control_fans.cpp
+++ Smart Home Automation System/hardware_interface/control_fans.cpp
@@ -1,1 +1,20 @@
+#include <Arduino.h>
+
+#define FAN_PIN 12 // Pin connected to the fan relay
+
+void setup() {
+  pinMode(FAN_PIN, OUTPUT);
+  Serial.begin(9600);
+}
+
+void loop() {
+  if (Serial.available() > 0) {
+    char command = Serial.read();
+    if (command == '1') {
+      digitalWrite(FAN_PIN, HIGH);  // Turn fan on
+    } else if (command == '0') {
+      digitalWrite(FAN_PIN, LOW);   // Turn fan off
+    }
+  }
+}
 
