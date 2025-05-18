a97bfc7333085916763db6fc1a95d83c8536d5e9
rahaan123 <85208811+rahaan123@users.noreply.github.com>
2025-05-18 18:40:13 -0500

    Update control_lights.cpp

--- Smart Home Automation System/hardware_interface/control_lights.cpp
+++ Smart Home Automation System/hardware_interface/control_lights.cpp
@@ -1,1 +1,19 @@
+#include <Arduino.h>
 
+#define LIGHT_PIN 13 // Pin connected to the light relay
+
+void setup() {
+  pinMode(LIGHT_PIN, OUTPUT);
+  Serial.begin(9600);
+}
+
+void loop() {
+  if (Serial.available() > 0) {
+    char command = Serial.read();
+    if (command == '1') {
+      digitalWrite(LIGHT_PIN, HIGH);  // Turn light on
+    } else if (command == '0') {
+      digitalWrite(LIGHT_PIN, LOW);   // Turn light off
+    }
+  }
+}
